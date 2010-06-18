c34f91d1317ecf4f9daf2f36c4b5bdbf45d66b6c
Prof Falken <theprofessor@threedrunkensysadsonthe.net>
2010-06-18 16:10:41 +0100

    updated steps file to create xml

--- features/steps/vhost_steps.rb
+++ features/steps/vhost_steps.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'libvirt'
 require 'xmlrpc/client'
 require 'net/ssh'
+require 'cobravsmongoose'
 
 # setup the globals
 
@@ -23,8 +24,55 @@ cblr_api = XMLRPC::Client.new(@cobbler_server,"/cobbler_api",@cobbler_port)
 
 Given /^that I want to build a server of type "([^"]*)"$/ do |serverType|
   # connect to cobbler and check the type exists
-  @xml_description = cblr_api.call("get_system_for_koan",serverType).inspect
-  puts @xml_description
+  @xml_description = cblr_api.call("get_system_for_koan",serverType)
+
+  virt_mem = @xml_description['virt_ram'] * 1024
+
+  puts <<-eos
+<domain type='kvm'>
+  <name>#{@xml_description['hostname']}</name>
+  <uuid></uuid>
+  <memory>#{virt_mem}</memory>
+  <currentMemory>#{virt_mem}</currentMemory>
+  <vcpu>#{@xml_description['virt_cpus']}</vcpu>
+  <os>
+    <type arch='x86_64' machine='pc-0.12'>hvm</type>
+    <boot dev='hd'/>
+  </os>
+  <features>
+    <acpi/>
+    <apic/>
+    <pae/>
+  </features>
+  <clock offset='utc'/>
+  <on_poweroff>destroy</on_poweroff>
+  <on_reboot>restart</on_reboot>
+  <on_crash>restart</on_crash>
+  <devices>
+    <emulator>/usr/bin/kvm</emulator>
+    <disk type='file' device='disk'>
+      <source file='/home/mwallace/.VirtualBox/kvm-images/icinga-disk0'/>
+      <target dev='hda' bus='ide'/>
+    </disk>
+    <interface type='bridge'>
+      <mac address='00:16:3e:5d:0f:6f'/>
+      <source bridge='virbr0'/>
+    </interface>
+    <console type='pty'>
+      <target port='0'/>
+    </console>
+    <console type='pty'>
+      <target port='0'/>
+    </console>
+    <input type='mouse' bus='ps2'/>
+    <graphics type='vnc' port='-1' autoport='yes' keymap='en-gb'/>
+    <video>
+      <model type='cirrus' vram='9216' heads='1'/>
+    </video>
+  </devices>
+</domain>
+
+eos
 end
 
 Then /^I should be able to connect to the provisioning server$/ do
