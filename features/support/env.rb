86e6de1883eb2e83a49b0c219f44827f689804f7
proffalken <theprofessor@threedrunkensysadsonthe.net>
2011-04-27 22:22:28 +0100

    Updated to use config.yml

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,46 +9,49 @@ require 'net/ssh'
 require 'ping'
 require 'socket'
 require 'timeout'
+require 'yaml'
 
 # setup the globals
 Before do
-#cobbler server:
-@cobbler_server = "localhost"
-@cobbler_port = "80"
+    # load in the YAML config
+    config_file = YAML::load(File.open('config.yml'))
+    #cobbler server:
+    @cobbler_server = config_file['cobbler']['server']
+    @cobbler_port = config_file['cobbler']['port']
 
-#Libvirt
-@libvirt_driver = "qemu" # can be any of qemu, vbox, xen, openvz, one, esx, gsx as detailed on the libvirt wiki
-@libvirt_host = ""
-@libvirt_type = "system"
-@libvirt_storage_pool = "default"
-
-
-## VM Connection
-@vmconn =  Libvirt::open("#{@libvirt_driver}://#{@libvirt_host}/#{@libvirt_type}")
-### VM Storage
-@vm_stor = @vmconn.lookup_storage_pool_by_name(@libvirt_storage_pool)   
+    #Libvirt
+    @libvirt_driver = config_file['libvirt']['driver']
+    @libvirt_host   = config_file['libvirt']['host']
+    @libvirt_type   = config_file['libvirt']['type']
+    @libvirt_storage_pool = config_file['libvirt']['storage_pool']
 
 
-## Cobbler API
-@cblr_api = XMLRPC::Client.new(@cobbler_server,"/cobbler_api",@cobbler_port)
+    ## VM Connection
+    @vmconn =  Libvirt::open("#{@libvirt_driver}://#{@libvirt_host}/#{@libvirt_type}")
+    ### VM Storage
+    @vm_stor = @vmconn.lookup_storage_pool_by_name(@libvirt_storage_pool)   
 
-### define the port-checker code
-def is_port_open(ip, port)
-  begin
-    Timeout::timeout(1) do
+
+    ## Cobbler API
+    @cblr_api = XMLRPC::Client.new(@cobbler_server,"/cobbler_api",@cobbler_port)
+
+    ### define the port-checker code
+    def is_port_open(ip, port)
       begin
-        s = TCPSocket.new(ip, port)
-        s.close
-        return true
-      rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
-        return false
+	Timeout::timeout(1) do
+	  begin
+	    s = TCPSocket.new(ip, port)
+	    s.close
+	    return true
+	  rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH
+	    return false
+	  end
+	end
+      rescue Timeout::Error
       end
-    end
-  rescue Timeout::Error
-  end
 
-  return false
-end
+      return false
+    end
 
 
 end
